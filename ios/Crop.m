a98798da00f6e005aea8994b563dc9a0998a00fb
Gabriel Policiuc <gabriel.policiuc@gmail.com>
2020-03-20 19:13:26 +0200

    Crop implementation

--- ios/Crop.m
+++ ios/Crop.m
@@ -1,14 +1,42 @@
 #import "Crop.h"
-
+#include <CoreGraphics/CGGeometry.h>
+#import <UIKit/UIKit.h>
 
 @implementation Crop
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(crop:(NSString *)base64
+                  x:(CGFloat)x
+                  y:(CGFloat)y
+                  width:(CGFloat)width
+                  height:(CGFloat)height
+                  callback:(RCTResponseSenderBlock)callback)
 {
-    // TODO: Implement some actually useful functionality
-    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+    @try
+    {
+        CGRect cropArea = CGRectMake(x, y, width, height);
+        UIImage* imageToCrop = [self getFromBase64:base64];
+        CGImageRef imageRef = CGImageCreateWithImageInRect([imageToCrop CGImage], cropArea);
+        UIImage* croppedImage = [UIImage imageWithCGImage:imageRef];
+        CGImageRelease(imageRef);
+        NSString *base64CroppedImage = [UIImagePNGRepresentation(croppedImage) base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
+        return callback(@[base64CroppedImage]);
+    }
+    @catch(NSException * exception)
+    {
+        return callback(@[@""]);
+    }
+}
+
+- (UIImage*) getFromBase64:(NSString*)base64
+{
+    if ([base64 hasPrefix:@"data:image/"] == NO) {
+        base64 = [@"data:image/png;base64," stringByAppendingString:base64];
+    }
+    NSURL *url = [NSURL URLWithString:base64];
+    NSData *originalImageData = [NSData dataWithContentsOfURL:url];
+    return [UIImage imageWithData:originalImageData];
 }
 
 @end
